8b88154811f1ce90ab452798a1ea7c8398b2f582
sutie <sususu_sutie@qq.com>
2023-02-28 19:18:24 +0800

    feat: react + antd test

---
+++ packages/react-comps/src/comps/Demo/index.tsx
@@ -0,0 +1,5 @@
+import { Typography } from 'antd';
+
+export default function Demo() {
+  return <Typography.Paragraph>demo</Typography.Paragraph>
+}
--- packages/react-comps/vite.config.ts
+++ packages/react-comps/vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react-swc'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/comps/index.ts'),
+      name: 'MyLib',
+      // the proper extensions will be added
+      fileName: 'my-lib',
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['react', 'antd'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          react: 'React',
+          antd: 'Antd'
+        },
+      },
+    },
+  },
 })
