8f7148639c50063db943ea55f9383a888014e88e
sutie <sususu_sutie@qq.com>
2023-03-07 11:34:52 +0800

    feat(react-comps): multi input, multi output

--- packages/react-comps/rollup.config.js
+++ packages/react-comps/rollup.config.js
@@ -1,35 +1,69 @@
 import json from "@rollup/plugin-json";
-import { resolve, dirname } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
-import typescript from 'rollup-plugin-typescript2';
-// import { name } from "./package.json";
+// import typescript2 from "rollup-plugin-typescript2";
+// import commonjs from "rollup-plugin-commonjs";
+import esbuild from "rollup-plugin-esbuild";
+// import resolve from "rollup-plugin-node-resolve";
+import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
+
+const outputDir = "./dist";
+const entryDir = "./src";
+
+const entries = fs.readdirSync(new URL(entryDir, import.meta.url));
+
+function parseEntry(entry) {
+  let filename, dirname, ext;
+  // 1. file: entry.ts, entry.tsx
+  if (/\.ts(x?)$/.test(entry)) {
+    filename = entry.replace(/\.ts(x?)$/, "");
+    dirname = "/";
+    ext = entry.match(/\.ts(x?)$/)[0].slice(1);
+  } else {
+    //  2. dir: entry => entry/index.ts, entry/index.tsx
+    const existTsx = fs.existsSync(
+      path.resolve(__dirname, `${entryDir}/${entry}/index.tsx`)
+    );
+    // const existTs = fs.existsSync(
+    //   path.resolve(__dirname, `${entryDir}/${entry}/index.ts`)
+    // );
+    filename = "index";
+    dirname = `/${entry}/`;
+    ext = existTsx ? "tsx" : "ts";
+  }
+
+  return { filename, dirname, ext };
+}
 
 /** @type {import('rollup').RollupOptions} */
-export default {
-  input: {
-    // index: resolve(__dirname, "./components/index.ts"),
-    // // fileURLToPath(new URL('./components/MyButton/index.tsx', import.meta.url)),
-    // MyButton: resolve(__dirname, "./components/MyButton/index.tsx"),
-    // MyTitle: resolve(__dirname, "./components/MyTitle/index.tsx"),
-    MyCalc: resolve(__dirname, "./components/MyCalc/index.ts"),
-  },
-  output: {
-    format: "es",
-    globals: {
-      react: "React",
-      "react-dom": "ReactDom",
-      antd: "Antd",
-    },
-  },
-  external: ["react", "react-dom", "antd"],
-  plugins: [
-    typescript(/*{ plugin options }*/),
-    json({
-      compact: true,
-    }),
-  ],
-  watch: false,
-};
+export default [
+  ...entries.map((entry) => {
+    const { filename, dirname, ext } = parseEntry(entry);
+    return {
+      input: `${entryDir}${dirname}${filename}.${ext}`,
+      output: {
+        format: "es",
+        exports: "named",
+        file: `${outputDir}${dirname}${filename}.js`,
+        globals: {
+          react: "React",
+          "react-dom": "ReactDom",
+          antd: "Antd",
+        },
+      },
+      external: ["react", "react-dom", "antd"],
+      plugins: [
+        // resolve(),
+        json({
+          compact: true,
+        }),
+        // commonjs(),
+        esbuild(),
+      ],
+      watch: false,
+    };
+  }),
+];
