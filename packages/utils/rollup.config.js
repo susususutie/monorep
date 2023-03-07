0619bb62b0833488742b0df9231bdc8d530562bb
sutie <sususu_sutie@qq.com>
2023-03-07 10:46:28 +0800

    feat(utils): finish

--- packages/utils/rollup.config.js
+++ packages/utils/rollup.config.js
@@ -2,25 +2,21 @@
 import terser from "@rollup/plugin-terser";
 // 支持引入json资源
 import json from "@rollup/plugin-json";
-//
+// 生成 .d.ts 文件
 import dts from "rollup-plugin-dts";
-import typescript2 from "rollup-plugin-typescript2";
-import resolve from "rollup-plugin-node-resolve";
+// import typescript2 from "rollup-plugin-typescript2";
 import commonjs from "rollup-plugin-commonjs";
+import esbuild from "rollup-plugin-esbuild";
+import resolve from "rollup-plugin-node-resolve";
 
-import { readFileSync, readdirSync } from "fs";
+import { readdirSync, readFileSync } from "fs";
 const pkg = JSON.parse(
   readFileSync(new URL("./package.json", import.meta.url))
 );
 
-const entry = readdirSync(new URL("./src", import.meta.url));
+const entries = readdirSync(new URL("./src", import.meta.url));
 
-/**
- *
- * @param {string} entry
- * @returns {import('rollup').RollupOptions}
- */
-function createConf(entry) {
+function parseEntry(entry) {
   let filename, path;
   if (/\.ts/.test(entry)) {
     filename = entry.replace(/\.ts/, "");
@@ -31,65 +27,78 @@ function createConf(entry) {
   }
 
   const input = `./src/${path}/${filename}.ts`.replace(/\/\//, "/");
-  console.log(entry, input);
+  const output = path || filename;
 
-  return [
-    // {
-    //   input,
-    //   output: [
-    //     {
-    //       format: "es",
-    //       exports: "named",
-    //       file: `./es/${path || filename}.d.ts`.replace(/\/\//, "/"),
-    //     },
-    //   ],
-    //   plugins: [resolve(), commonjs(), json(), typescript2()],
-    // },
-    {
-      input, // 入口文件
-      output: [
-        {
-          format: "umd",
-          // 导出多个还是导出一个, 不建议混用:
-          // named:   具名导出, 多个
-          // default: 默认导出, 一个
-          exports: "named",
-          name: "SutieUtils", // window.SutieUtils
-          file: `./umd/${path || filename}.js`.replace(/\/\//, "/"),
-        },
-        {
-          format: "es",
-          exports: "named",
-          file: `./es/${path || filename}.js`.replace(/\/\//, "/"),
-        },
-        {
-          format: "es",
-          exports: "named",
-          file: `./es/${path || filename}.d.ts`.replace(/\/\//, "/"),
-        },
-        {
-          format: "cjs",
-          exports: "named",
-          file: `./cjs/${path || filename}.cjs`.replace(/\/\//, "/"),
-          // 插件可以写在 output 针对某一种输出生效
-          plugins: [terser()],
-        },
-      ],
+  const umdFile = `./umd/${output}.js`.replace(/\/\//, "/");
+  const esmFile = `./es/${output}.js`.replace(/\/\//, "/");
+  const cjsFile = `./cjs/${output}.cjs`.replace(/\/\//, "/");
+  const dtsFile = `./es/${path || filename}.d.ts`.replace(/\/\//, "/");
 
-      // 插件也可以对所有输出生效
-      plugins: [
-        resolve(),
-        commonjs(),
-        json(),
-        typescript2({
-          tsconfigOverride: {
-            declaration: true,
-            emitDeclarationOnly: true,
-          },
-        }),
-      ],
+  return { input, umdFile, esmFile, cjsFile, dtsFile };
+}
+
+/**
+ *
+ * @param {string} entry
+ * @returns {import('rollup').RollupOptions}
+ */
+function createConf(entry) {
+  const { input, umdFile, esmFile, cjsFile } = parseEntry(entry);
+
+  // console.log(`${entry} ==> ${input}`);
+
+  return {
+    input, // 入口文件
+    output: [
+      {
+        format: "umd",
+        // 导出多个还是导出一个, 不建议混用:
+        // named:   具名导出, 多个
+        // default: 默认导出, 一个
+        exports: "named",
+        name: "SutieUtils", // window.SutieUtils
+        file: umdFile,
+      },
+      {
+        format: "es",
+        exports: "named",
+        file: esmFile,
+      },
+      {
+        format: "cjs",
+        exports: "named",
+        file: cjsFile,
+        // 插件可以写在 output 针对某一种输出生效
+        plugins: [terser()],
+      },
+    ],
+
+    // 插件也可以对所有输出生效
+    plugins: [
+      resolve(),
+      json(),
+      commonjs(),
+      esbuild(), // 或者 typescript2(),
+    ],
+  };
+}
+
+function createConfDts(entry) {
+  const { input, dtsFile } = parseEntry(entry);
+
+  return {
+    input,
+    output: {
+      format: "es",
+      exports: "named",
+      file: dtsFile,
     },
-  ];
+
+    plugins: [
+      json(),
+      dts({ respectExternal: true }),
+    ],
+  };
 }
 
 // console.log(
@@ -100,7 +109,10 @@ function createConf(entry) {
 //   )
 // );
 
-export default entry.map((name) => createConf(name)).flat();
+export default [
+  ...entries.map((name) => createConf(name)),
+  ...entries.map((name) => createConfDts(name)),
+];
 
 /**
  * output.exports
---
+++ packages/utils/src/index.ts
@@ -0,0 +1,5 @@
+export { default as version } from "./version";
+export { default as main } from "./main";
+export { default as other } from "./other";
+export { default as dir } from "./dir";
+
