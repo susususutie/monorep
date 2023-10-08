1ad74ee8ff993912f730b200d4261babc3781b1a
sutie <sususu_sutie@qq.com>
2023-10-08 14:44:59 +0800

    feat(antd): BarTitle

--- packages/antd/src/BarTitle/demo.tsx
+++ packages/antd/src/BarTitle/demo.tsx
@@ -9,13 +9,25 @@ export default function Demo() {
 
       <Divider />
 
-      <Card >
+      <Card>
         <Space direction="vertical">
           <BarTitle>默认</BarTitle>
-          <BarTitle type='danger'>danger</BarTitle>
-          <BarTitle type='secondary'>secondary</BarTitle>
-          <BarTitle type='success'>success</BarTitle>
-          <BarTitle type='warning'>warning</BarTitle>
+          <BarTitle type="danger">danger</BarTitle>
+          <BarTitle type="secondary">secondary</BarTitle>
+          <BarTitle type="success">success</BarTitle>
+          <BarTitle type="warning">warning</BarTitle>
+        </Space>
+      </Card>
+
+      <Divider />
+
+      <Card title="自定义颜色">
+        <Space direction="vertical">
+          <BarTitle color='blue'>blue</BarTitle>
+          <BarTitle color="pink">pink</BarTitle>
+          <BarTitle color="lime">lime</BarTitle>
+          <BarTitle color="green">green</BarTitle>
+          <BarTitle color="red">red</BarTitle>
         </Space>
       </Card>
     </>
