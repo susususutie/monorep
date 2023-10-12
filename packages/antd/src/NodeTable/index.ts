50d019d297ff67df333f217de32df2ea4aa75047
sutie <sususu_sutie@qq.com>
2023-10-12 16:35:51 +0800

    feat: NodeTable

---
+++ packages/antd/src/NodeTable/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./NodeTable";
+export * from "./NodeTable";
--- packages/antd/src/index.ts
+++ packages/antd/src/index.ts
@@ -1,12 +1,14 @@
-export { default as Arrow } from './Arrow';
-export type { ArrowProps, ArrowDirection, ArrowColor } from './Arrow';
-export type { ArrowGroupProps } from './Arrow/Group';
-export { default as AsyncSelect } from './AsyncSelect';
-export type { AsyncSelectProps } from './AsyncSelect';
-export { default as ComponentPanel } from './ComponentPanel';
-export type { ComponentPanelProps } from './ComponentPanel';
-export { default as FlyLine } from './FlyLine'
-export { default as MyButton } from './MyButton';
-export type { MyButtonProps } from './MyButton';
-export { default as BarTitle } from './BarTitle';
-export type { BarTitleProps as BarTitleProps } from './BarTitle';
+export { default as Arrow } from "./Arrow";
+export type { ArrowProps, ArrowDirection, ArrowColor } from "./Arrow";
+export type { ArrowGroupProps } from "./Arrow/Group";
+export { default as AsyncSelect } from "./AsyncSelect";
+export type { AsyncSelectProps } from "./AsyncSelect";
+export { default as ComponentPanel } from "./ComponentPanel";
+export type { ComponentPanelProps } from "./ComponentPanel";
+export { default as FlyLine } from "./FlyLine";
+export { default as MyButton } from "./MyButton";
+export type { MyButtonProps } from "./MyButton";
+export { default as NodeTable } from "./NodeTable";
+export type { MatrixTableProps as NodeTableProps } from "./NodeTable";
+export { default as BarTitle } from "./BarTitle";
+export type { BarTitleProps as BarTitleProps } from "./BarTitle";
