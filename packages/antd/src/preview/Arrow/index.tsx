a4a3fd8f6425a6c7416423462adae10bc5336011
sutie <sususu_sutie@qq.com>
2024-11-12 20:20:54 +0800

    fixup

--- packages/antd/src/preview/Arrow/index.tsx
+++ packages/antd/src/preview/Arrow/index.tsx
@@ -14,28 +14,28 @@ export default function DemoArrow() {
             title: "方向",
             description: "direction参数控制箭头方向",
             component: () => import("./direction"),
-            originCode: () => import("./direction"),
+            originCode: () => import("./direction?raw"),
           },
           {
             key: 2,
             title: "颜色",
             description: "color参数控制箭头颜色",
             component: () => import("./color"),
-            originCode: () => import("./color"),
+            originCode: () => import("./color?raw"),
           },
           {
             key: 3,
             title: "动画",
             description: "animation参数控制显示动画效果",
             component: () => import("./animation"),
-            originCode: () => import("./animation"),
+            originCode: () => import("./animation?raw"),
           },
           {
             key: 4,
             title: "分组",
             description: "通过Group分组, 可将多个箭头堆叠",
             component: () => import("./group"),
-            originCode: () => import("./group"),
+            originCode: () => import("./group?raw"),
           },
         ]}
       />
